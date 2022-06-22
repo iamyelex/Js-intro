2e112285edbe4c6cfdb1775bf13b37765cd4d73a
iamyelex <yelex4love@gmail.com>
2022-06-23 05:28:59 +0800

    work updated to console

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-// const myName = "Akinsipe Hammed"
-// document.body.innerHTML = `<h1> My Name is  ${myName} </h1>`
-
-// const myHeight = 130
-// document.body.innerHTML = `<h1> My Height is  ${myHeight} </h1>`
-
-// const myCountry = "Nigeria"
-// document.body.innerHTML = `<h1> My Country is  ${myCountry} </h1>`
-
-
-// console.log("My name is " + myName)
-// console.log(`My Height is ${myHeight}`)
-// console.log(`The name of my country is ${myCountry}`)
-
 const myName = "Akinsipe Hammed"
-document.write(`<h1> My Name is  ${myName} </h1>`)
-
 const myHeight = "5'7"
-document.write(`<h1> My Height is  ${myHeight} </h1>`)
-
 const myCountry = "Nigeria"
-document.write (`<h1> My Country is  ${myCountry} </h1>`)
+
+console.log(myName)
+console.log(myHeight)
+console.log(myCountry)
